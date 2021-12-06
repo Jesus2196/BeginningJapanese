5c8a3e9346555af7c251367854d92b62b5351e2a
Jesus <Jesuspineda9512@gmail.com>
2021-12-06 11:13:02 -0800

    HiraganaCards and CSS

--- src/components/HiraganaCard/HiraganaCard.jsx
+++ src/components/HiraganaCard/HiraganaCard.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import './HiraganaCard.css';
 
 export default function HiraganaCard({ character }) {
     return (
         <div>
-            {character.character}
+            {/* {character.character} */}
+            <div className="column">
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img
+                                className="front-card"
+                                src={`${character.img_path}`}
+                                alt={`${character.character}`}
+                            />
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>{character.character}</h1>
+                            <img
+                                className="back-card"
+                                src={`${character.strokeOrder}`}
+                                alt={`${character.character}`}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
